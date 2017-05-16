40b16c19ed688bf7258643ebe65f9ef9e5c39568
Dmitriy Savin <its2exp@gmail.com>
2017-05-16 17:05:51 +0300

    Add basic API wrapper for imgur

---
+++ app/services/Api.js
@@ -0,0 +1,29 @@
+import apisauce from 'apisauce';
+
+const BASE_URL = 'https://api.imgur.com/3/';
+const FIRST_PAGE = 0;
+const DEFAULT_SORT = 'hot';
+
+const create = () => {
+  const api = apisauce.create({
+    baseURL: BASE_URL,
+    headers: {
+      'Authorization': 'Client-ID {{CLIENT_ID}}',
+    },
+    // 10 second timeout...
+    timeout: 10000,
+    responseType: 'json',
+  });
+  //
+  const getGallery = ({ section, sort = DEFAULT_SORT, page = FIRST_PAGE }) => {
+    return api
+      .get(`gallery/${section}/${sort}/${page}`)
+      .then(response => response);
+  };
+
+  return {
+    getGallery,
+  };
+};
+
+export default { create };
